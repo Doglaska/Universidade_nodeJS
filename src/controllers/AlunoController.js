ab05a34548fc1f0f0646b19615c2e998e43dd8d4
Doglaska <doglaska@gmail.com>
2026-09-08 19:01:09 -0300

    feat: Adicionando o controle da API

---
+++ src/controllers/AlunoController.js
@@ -0,0 +1,29 @@
+const AlunoService = require("../services/AlunoService")
+
+class AlunoController{
+    findMany(req, res){
+        const alunos = AlunoService.findMany();
+        return res.status(200).json({alunos});
+    }
+
+    create(req, res){
+        const aluno = AlunoService.create(req.body);
+    
+        if(!aluno){
+            return res.status(400).json({error: "Campos nome, email, nota1 e nota2 são obrigatorios"});
+        }
+        return res.status(201).json({aluno});
+    }
+
+    delete(req, res){
+        const {id} = req.params;
+        const aluno = AlunoService.delete(id);
+
+        if(!aluno){
+            return res.status(404).json({error: "Aluno não encontrado"});
+        }
+        return res.status(204).end();
+    }
+}
+
+module.exports = new AlunoController;
