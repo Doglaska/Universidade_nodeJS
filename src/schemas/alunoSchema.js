be4eff91b44648119f7b1eb56c5643f790a4819e
Doglaska <doglaska@gmail.com>
2026-09-22 16:32:27 -0300

    chore: install zod validacao

---
+++ src/schemas/alunoSchema.js
@@ -0,0 +1,7 @@
+const z = require("zod");
+const alunoSchema = z.object({
+    nome: z.string().trim().min(3, "Nome muito curto."),
+    email: z.string().trim().email("E-mail Inválido.")
+});
+
+module.exports = alunoSchema;
